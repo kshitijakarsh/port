d54d012491845b6be1369cd9d3d1a1583e9e9143
kshitijakarsh <kshitijakarsh@gmail.com>
2026-03-26 09:42:55 +0530

    feat: Add animated MotionButton component and integrate it into ProjectCard.

---
+++ src/components/MotionButton.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import { MoveUpRight } from "lucide-react";
+import { motion } from "motion/react";
+
+export default function MotionButton() {
+  return (
+    <motion.button
+      whileHover="hover"
+      whileTap="tap"
+      className="group bg-muted text-muted-foreground hover:text-foreground outline-border flex items-center justify-center gap-2 rounded-sm px-2 py-1 font-sans text-xs outline-1 outline-offset-2 transition-colors"
+    >
+      <motion.div
+        variants={{
+          hover: { scale: 1.2, rotate: 45 },
+          tap: { scale: 0.8 },
+        }}
+        transition={{ duration: 0.2 }}
+      >
+        <MoveUpRight size={10} />
+      </motion.div>
+    </motion.button>
+  );
+}
--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -2,7 +2,8 @@ import { Project } from "@/types";
 import Image from "next/image";
 import Button from "./Button";
 import { MoveUpRight } from "lucide-react";
-
+import { motion } from "motion/react";
+import MotionButton from "./MotionButton";
 
 export default function ProjectCard(props: Project) {
   return (
@@ -14,25 +15,23 @@ export default function ProjectCard(props: Project) {
           width={0}
           height={0}
           sizes="100vw"
-          className="w-full h-auto object-contain"
+          className="h-auto w-full object-contain"
           priority
         />
-
-
       </div>
 
       <div className="flex flex-col gap-3 px-1">
         <div className="flex flex-col gap-1 px-2">
           <div className="flex justify-between">
-            <h3 className="text-xl font-serif leading-tight">
-              {props.title}
-            </h3>
+            <h3 className="font-serif text-xl leading-tight">{props.title}</h3>
 
-            <Button variant="social" href={`/projects/${props.id}`}>
+            {/* <Button variant="social" href={`/projects/${props.id}`}>
               <MoveUpRight size={10} />
-            </Button>
+            </Button> */}
+
+            <MotionButton />
           </div>
-          <p className="text-sm text-muted-foreground line-clamp-2 leading-relaxed">
+          <p className="text-muted-foreground line-clamp-2 text-sm leading-relaxed">
             {props.description}
           </p>
         </div>
